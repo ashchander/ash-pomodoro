07411f28d0b1bce5bc4580c5db4ac6c244dabb37
Ash Chander <ashchander@gmail.com>
2023-12-28 14:38:24 -0500

    break out ticker logic

--- main.go
+++ main.go
@@ -5,32 +5,33 @@ import (
 	"time"
 )
 
-func main() {
+func handleTicker(ticker *time.Ticker) {
 	onBreak := false
+	counter := 0
+	for {
+		<-ticker.C
+		counter++
+		fmt.Println("counter at ", counter)
+		if !onBreak && counter == 25 {
+			onBreak = true
+			counter = 0
+			fmt.Println("Done work")
+			fmt.Print("\x07")
+		} else if onBreak && counter == 5 {
+			onBreak = false
+			counter = 0
+			fmt.Println("Done break")
+			fmt.Print("\x07")
+		}
+	}
+}
 
+func main() {
 	// TODO: Change this to minutes
 	ticker := time.NewTicker(time.Second)
 	defer ticker.Stop()
-	counter := 0
 
-	go func() {
-		for {
-			<-ticker.C
-			counter++
-			fmt.Println("counter at ", counter)
-			if !onBreak && counter == 25 {
-				onBreak = true
-				counter = 0
-				fmt.Println("Done work")
-				fmt.Print("\x07")
-			} else if onBreak && counter == 5 {
-				onBreak = false
-				counter = 0
-				fmt.Println("Done break")
-				fmt.Print("\x07")
-			}
-		}
-	} ()
+	go handleTicker(ticker)
 
 	time.Sleep(time.Minute)
 }
