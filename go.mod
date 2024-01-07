d04049a00ec384c73cec7ed5f7c3a27abb91dc9a
Ash Chander <ashchander@gmail.com>
2024-01-07 10:14:31 -0500

    initial bubbletea implmementation

--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module ashbuilds.xyz/pomodoro
 
 go 1.15
+
+require (
+	github.com/charmbracelet/bubbletea v0.25.0 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/muesli/ansi v0.0.0-20230316100256-276c6243b2f6 // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
+	golang.org/x/sync v0.6.0 // indirect
+	golang.org/x/term v0.16.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
--- main.go
+++ main.go
@@ -7,21 +7,58 @@ import (
 	"os"
 	"strings"
 	"time"
+
+	tea "github.com/charmbracelet/bubbletea"
 )
 
-var paused bool
-var counter uint8
+type pomo struct {
+	paused	bool
+	counter uint8
+	status	string
+	ticker	*time.Ticker
+}
+
+func initializeModel() pomo {
+	return pomo {
+		paused: false,
+		counter: 0,
+		status: "pomodore",
+		ticker: time.NewTicker(time.Second),
+	}
+}
+
+func (p pomo) Init() tea.Cmd {
+	// No Initial I/O
+	return nil
+}
+
+func (p pomo) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	// Keypress?
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl-c", "q":
+			return p, tea.Quit
+		case "space", "p":
+			p.paused = true
+		case "r":
+			p.counter = 0
+			p.status = "pomodore"
+			p.paused = false
+		}
+	}
+	return p, nil
+}
+
+func (p pomo) View() string {
+	// TODO Implement view code
+	return ""
+}
 
 func beep(text string) {
 	fmt.Println(text)
 	fmt.Print("\x07")
-}
-
-func readInput(reader *bufio.Reader) (string, error)  {
-	fmt.Print("Enter a command: ")
-	text, err := reader.ReadString('\n')
-	text = strings.TrimSpace(text)
-	return text, err
 }
 
 func handleTicker(ticker *time.Ticker) {
@@ -48,20 +85,12 @@ func handleTicker(ticker *time.Ticker) {
 
 func main() {
 	// TODO: Change this to minutes
-	ticker := time.NewTicker(time.Second)
-	defer ticker.Stop()
+	pomodoro := initializeModel()
+	defer pomodoro.ticker.Stop()
 
 	go handleTicker(ticker)
 
-	reader := bufio.NewReader(os.Stdin)
-	var text string
-	var err error
 	for text != "quit" {
-		text, err = readInput(reader)
-		if err != nil {
-			log.Fatal("Could not read input")
-		}
-
 		if text == "pause" {
 			paused = true
 		} else if text == "unpause" {
